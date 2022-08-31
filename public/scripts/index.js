75fc9ccd8a64076b1ea32a4d88cb9baa5527755c
Avigail Siman Tov <avigailsi123@gmail.com>
2022-08-31 22:37:23 +0300

    רוב הפרויקט

---
+++ public/scripts/index.js
@@ -0,0 +1,37 @@
+// const axios = require('axios').default;
+
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+// app.use('/', express.static('enter.ejs'))
+app.use(express.json())
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req,res) => {
+    res.render('enter');        
+});
+
+app.get('/enter2', (req,res) => {
+    res.render('enter2');        
+});
+app.get('/page', (req,res) => {
+    res.render('page');        
+});
+app.get('/game_over', (req,res) => {
+    res.render('game_over');        
+});
+app.get('/errors', (req,res) => {
+    res.render('errors');        
+});
+app.get('/win', (req,res) => {
+    res.render('win');        
+});
+const port =  process.env.PORT || 3001
+app.listen(port, () => {    
+    console.log(`\nserver is up and running at: http://127.0.0.1:${port}\n` )
+})
+
+
+
---
+++ public/scripts/level.js
@@ -0,0 +1,18 @@
+//A function intended for the light level
+function easy(){
+  localStorage.setItem("levelPlay", "1");//Keeps me level
+  location.href='page'//Redirects to the level page
+}
+//A function intended for the intermediate level
+function normal(){
+  localStorage.setItem("levelPlay", "2");//Keeps me level
+  location.href='page';//Redirects to the level page
+}
+//A function designed for the hard level
+function hard(){
+  localStorage.setItem("levelPlay", "3");//Keeps me level
+  location.href='page';//Redirects to the level page
+}
+
+
+
